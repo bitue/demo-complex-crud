ae246a865ad0ddd8f0ed6aa85706ed87e2e3d208
bitue <ashikul.islam_ashik@yahoo.com>
2025-03-03 19:33:00 -0800

    aa

--- src/main/java/com/example/demoSkh/demoSkh/config/ModelMapperConfig.java
+++ src/main/java/com/example/demoSkh/demoSkh/config/ModelMapperConfig.java
@@ -1,5 +1,4 @@
 package com.example.demoSkh.demoSkh.config;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/demoSkh/demoSkh/dto/AppointmentResponseDto.java
+++ src/main/java/com/example/demoSkh/demoSkh/dto/AppointmentResponseDto.java
@@ -1,5 +1,4 @@
 package com.example.demoSkh.demoSkh.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
